11c13cd5dd58d1894c09345e1a5b6531d9a8aa36
arlojay <35749102+arlojay@users.noreply.github.com>
2025-06-03 17:20:15 -0500

    Update recipes to new system

--- index.js
+++ index.js
@@ -31,75 +31,90 @@ async function main() {
     }
 
     // Primary colors
-    const magenta = mod.createCraftingRecipe("magenta_pigment").createShapeless(colors.magenta, 2);
-    magenta.addItem(colors.rose, 1); magenta.addItem(colors.violet, 1);
+    mod.createCraftingRecipe("magenta_pigment", colors.magenta, 2)
+        .addItem(colors.rose, 1)
+        .addItem(colors.violet, 1);
     
-    const yellow = mod.createCraftingRecipe("yellow_pigment").createShapeless(colors.yellow, 2);
-    yellow.addItem(colors.lime, 1); yellow.addItem(colors.orange, 1);
+    mod.createCraftingRecipe("yellow_pigment", colors.yellow, 2)
+        .addItem(colors.lime, 1)
+        .addItem(colors.orange, 1);
     
-    const cyan = mod.createCraftingRecipe("cyan_pigment");
-    const cyanA = cyan.createShapeless(colors.cyan, 2);
-    cyanA.addItem(colors.green, 1); cyanA.addItem(colors.blue, 1);
-    const cyanB = cyan.createShapeless(colors.cyan, 2);
-    cyanB.addItem(colors.spring_green, 1); cyanB.addItem(colors.azure, 1);
+    mod.createCraftingRecipe("cyan_pigment_a", colors.cyan, 2)
+        .addItem(colors.green, 1)
+        .addItem(colors.blue, 1);
+    mod.createCraftingRecipe("cyan_pigment_b", colors.cyan, 2)
+        .addItem(colors.spring_green, 1)
+        .addItem(colors.azure, 1);
 
     // Secondary colors
-    const red = mod.createCraftingRecipe("red_pigment").createShapeless(colors.red, 2);
-    red.addItem(colors.magenta, 1); red.addItem(colors.yellow, 1);
+    mod.createCraftingRecipe("red_pigment", colors.red, 2)
+        .addItem(colors.magenta, 1)
+        .addItem(colors.yellow, 1);
     
-    const blue = mod.createCraftingRecipe("blue_pigment").createShapeless(colors.blue, 2);
-    blue.addItem(colors.magenta, 1); blue.addItem(colors.cyan, 1);
+    mod.createCraftingRecipe("blue_pigment", colors.blue, 2)
+        .addItem(colors.magenta, 1)
+        .addItem(colors.cyan, 1);
     
-    const green = mod.createCraftingRecipe("green_pigment");
-    const greenA = green.createShapeless(colors.green, 2);
-    greenA.addItem(colors.yellow, 1); greenA.addItem(colors.cyan, 1);
-    const greenB = green.createShapeless(colors.green, 2);
-    greenB.addItem(colors.yellow, 1); greenB.addItem(colors.blue, 1);
+    mod.createCraftingRecipe("green_pigment_a", colors.green, 2)
+        .addItem(colors.yellow, 1)
+        .addItem(colors.cyan, 1);
+    mod.createCraftingRecipe("green_pigment_a", colors.green, 2)
+        .addItem(colors.yellow, 1)
+        .addItem(colors.blue, 1);
 
     // Tertiary colors
-    const orange = mod.createCraftingRecipe("orange_pigment").createShapeless(colors.orange, 2);
-    orange.addItem(colors.red, 1); orange.addItem(colors.yellow, 1);
+    mod.createCraftingRecipe("orange_pigment", colors.orange, 2)
+        .addItem(colors.red, 1)
+        .addItem(colors.yellow, 1);
     
-    const lime = mod.createCraftingRecipe("lime_pigment").createShapeless(colors.lime, 2);
-    lime.addItem(colors.yellow, 1); lime.addItem(colors.green, 1);
+    mod.createCraftingRecipe("lime_pigment", colors.lime, 2)
+        .addItem(colors.yellow, 1)
+        .addItem(colors.green, 1);
     
-    const spring_green = mod.createCraftingRecipe("spring_green_pigment").createShapeless(colors.spring_green, 2);
-    spring_green.addItem(colors.green, 1); spring_green.addItem(colors.cyan, 1);
+    mod.createCraftingRecipe("spring_green_pigment", colors.spring_green, 2)
+        .addItem(colors.green, 1)
+        .addItem(colors.cyan, 1);
     
-    const azure = mod.createCraftingRecipe("azure_pigment").createShapeless(colors.azure, 2);
-    azure.addItem(colors.cyan, 1); azure.addItem(colors.blue, 1);
+    mod.createCraftingRecipe("azure_pigment", colors.azure, 2)
+        .addItem(colors.cyan, 1)
+        .addItem(colors.blue, 1);
     
-    const violet = mod.createCraftingRecipe("violet_pigment");
-    const violetA = violet.createShapeless(colors.violet, 2);
-    violetA.addItem(colors.blue, 1); violetA.addItem(colors.magenta, 1);
-    const violetB = violet.createShapeless(colors.violet, 2);
-    violetB.addItem(colors.blue, 1); violetB.addItem(colors.red, 1);
+    mod.createCraftingRecipe("violet_pigment_a", colors.violet, 2)
+        .addItem(colors.blue, 1)
+        .addItem(colors.magenta, 1);
+    mod.createCraftingRecipe("violet_pigment_b", colors.violet, 2)
+        .addItem(colors.blue, 1)
+        .addItem(colors.red, 1);
     
-    const rose = mod.createCraftingRecipe("rose_pigment").createShapeless(colors.rose, 2);
-    rose.addItem(colors.magenta, 1); rose.addItem(colors.red, 1);
+    mod.createCraftingRecipe("rose_pigment", colors.rose, 2)
+        .addItem(colors.magenta, 1)
+        .addItem(colors.red, 1);
 
     // Grays
-    const gray = mod.createCraftingRecipe("gray_pigment");
-    const grayA = gray.createShapeless(colors.gray, 2);
-    grayA.addItem(colors.black, 1); grayA.addItem(colors.white, 1);
-    const grayB = gray.createShapeless(colors.gray, 2);
-    grayB.addItem(colors.dark_gray, 1); grayB.addItem(colors.light_gray, 1);
+    mod.createCraftingRecipe("gray_pigment_a", colors.gray, 2)
+        .addItem(colors.black, 1)
+        .addItem(colors.white, 1);
+    mod.createCraftingRecipe("gray_pigment_a", colors.gray, 2)
+        .addItem(colors.dark_gray, 1)
+        .addItem(colors.light_gray, 1);
     
-    const darkGray = mod.createCraftingRecipe("dark_gray_pigment");
-    const darkGrayA = darkGray.createShapeless(colors.dark_gray, 2);
-    darkGrayA.addItem(colors.black, 1); darkGrayA.addItem(colors.gray, 1);
-    const darkGrayB = darkGray.createShapeless(colors.dark_gray, 2);
-    darkGrayB.addItem(colors.black, 1); darkGrayB.addItem(colors.light_gray, 1);
+    mod.createCraftingRecipe("dark_gray_pigment_a", colors.dark_gray, 2)
+        .addItem(colors.black, 1)
+        .addItem(colors.gray, 1);
+    mod.createCraftingRecipe("dark_gray_pigment_b", colors.dark_gray, 2)
+        .addItem(colors.black, 1)
+        .addItem(colors.light_gray, 1);
     
-    const lightGray = mod.createCraftingRecipe("light_gray_pigment");
-    const lightGrayA = lightGray.createShapeless(colors.light_gray, 2);
-    lightGrayA.addItem(colors.white, 1); lightGrayA.addItem(colors.gray, 1);
-    const lightGrayB = lightGray.createShapeless(colors.light_gray, 2);
-    lightGrayB.addItem(colors.white, 1); lightGrayB.addItem(colors.dark_gray, 1);
+    mod.createCraftingRecipe("light_gray_pigment_a", colors.light_gray, 2)
+        .addItem(colors.white, 1)
+        .addItem(colors.gray, 1);
+    mod.createCraftingRecipe("light_gray_pigment_a", colors.light_gray, 2)
+        .addItem(colors.white, 1)
+        .addItem(colors.dark_gray, 1);
 
 
-    mod.createCraftingRecipe("white_pigment").createShapeless(colors.white, 1).addItem("base:latex", 1);
-    mod.createCraftingRecipe("black_pigment").createShapeless(colors.black, 1).addItem("base:rubber_ball", 1);
+    mod.createCraftingRecipe("white_pigment", colors.white, 1).addItem("base:latex", 1);
+    mod.createCraftingRecipe("black_pigment", colors.black, 1).addItem("base:rubber_ball", 1);
 
 
 
